57614fc785bd57287e791f94cece042f49f9a238
Ivan Alvarez <sistemas.ialvarez93@gmail.com>
2025-02-19 16:34:20 -0400

    chore(ecosystem): add logs and watch for the config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,6 +4,12 @@ module.exports = {
       name: "agenda-cron",
       script: "./bin/www",
       max_memory_restart: "1G",
+      watch: true,
+      ignore_watch: ["./node_modules", "./logs"],
+      error_file: path.resolve(__dirname, "./logs/pm2-err.log"),
+      out_file: path.resolve(__dirname, "./logs/pm2-out.log"),
+      log_file: path.resolve(__dirname, "./logs/pm2-combined.log"),
+      time: true,
     },
   ],
 };
